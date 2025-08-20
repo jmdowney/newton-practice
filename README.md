012cc83203a9dafd7c41e2af2d55fd5fdc2c91ba
Jordan Downey <jordandowney@berkeley.edu>
2025-08-20 10:16:19 -0700

    Add basic implementation of Newton's method.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # newton-practice
-Test repository of Newton's method
+Test repository of Newton's method; basic implementation of Newton's method
---
+++ newton.py
@@ -0,0 +1,13 @@
+import numpy as py
+
+def derivative(fun, start, eps = 1e-7):
+    return (fun(start + eps) - fun(start - eps)) / (2 * eps)
+    
+
+def optimize(start, fun, eps = 1e-7, max_iter = 100):
+    x = start
+    for i in range(max_iter):
+        x.t = start - derivative(fun, start, eps) / derivative(derivative(fun, start, eps), start, eps)
+        x = x + x.t
+    return x
+    
